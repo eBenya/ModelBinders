876fa614a182927825d7455316f59255a4ebdc84
eBenya <koly_07@mail.ru>
2023-03-10 19:59:52 +0300

    Init repo

---
+++ Examples/JsonWithFilesBindingExample/Controllers/ExampleController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JsonWithFilesBindingExample.Controllers;
+
+[ApiController]
+[Route("api")]
+public class ExampleController : Controller
+{
+    private readonly ILogger<ExampleController> _logger;
+
+    public ExampleController(ILogger<ExampleController> logger)
+    {
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+
+    [HttpPost("do-some-with-data")]
+    public IActionResult DoSomethingWithThisData(ExampleRequestModel requestModel)
+    {
+        return Ok("Файлы с моделью обработаны");
+    }
+    
+    [HttpGet("check")]
+    public IActionResult Test()
+    {
+        return Ok("Ok");
+    }
+}
+
+//[ModelBinder(typeof(JsonWithFilesFormModelBinder), Name = nameof(ExampleRequestModel))]
+public record ExampleRequestModel
+{
+    public string Name { get; init; }
+    public int Age { get; init; }
+     
+    public IFormFile File { get; init; }
+    public ICollection<IFormFile> OtherFiles { get; init; }
+}
---
+++ src/ModelBinders/JsonWithFilesFormModelBinder.cs
@@ -0,0 +1,67 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+using Microsoft.Extensions.Logging;
+
+namespace ModelBinders.MVC;
+
+public class JsonWithFilesFormModelBinder : IModelBinder
+{
+	private readonly FormFileModelBinder _formFileModelBinder;
+
+	public JsonWithFilesFormModelBinder( ILoggerFactory loggerFactory)
+	{
+		_formFileModelBinder = new FormFileModelBinder(loggerFactory);
+	}
+
+	public async Task BindModelAsync(ModelBindingContext bindingContext)
+	{
+		if (bindingContext == null)
+			throw new ArgumentNullException(nameof(bindingContext));
+
+		// Retrieve the form part containing the JSON
+		var valueResult = bindingContext.ValueProvider.GetValue(bindingContext.FieldName);
+		if (valueResult == ValueProviderResult.None)
+		{
+			var message = bindingContext.ModelMetadata.ModelBindingMessageProvider.MissingBindRequiredValueAccessor(bindingContext.FieldName);
+			bindingContext.ModelState.TryAddModelError(bindingContext.ModelName, message);
+			return;
+		}
+		var rawValue = valueResult.FirstValue;
+
+		// Deserialize the JSON
+		var model = JsonSerializer.Deserialize(rawValue, bindingContext.ModelType);
+
+		// Bind each of the IFormFile properties from the other form parts
+		foreach (var property in bindingContext.ModelMetadata.Properties)
+		{
+			if (property.ModelType != typeof(ICollection<IFormFile>) && property.ModelType != typeof(IFormFile))
+				continue;
+
+			var fieldName = property.BinderModelName ?? property.PropertyName;
+			var modelName = fieldName;
+			var propertyModel = property.PropertyGetter(bindingContext.Model);
+			ModelBindingResult propertyResult;
+			using (bindingContext.EnterNestedScope(property, fieldName, modelName, propertyModel))
+			{
+				await _formFileModelBinder.BindModelAsync(bindingContext).ConfigureAwait(false);
+				propertyResult = bindingContext.Result;
+			}
+
+			if (propertyResult.IsModelSet)
+			{
+				// The IFormFile was sucessfully bound, assign it to the corresponding property of the model
+				property.PropertySetter(model, propertyResult.Model);
+			}
+			else if (property.IsBindingRequired)
+			{
+				var message = property.ModelBindingMessageProvider.MissingBindRequiredValueAccessor(fieldName);
+				bindingContext.ModelState.TryAddModelError(modelName, message);
+			}
+		}
+
+		// Set the successfully constructed model as the result of the model binding
+		bindingContext.Result = ModelBindingResult.Success(model);
+	}
+}
